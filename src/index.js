80336d028aa5511fbd8bbe27b3dbc9374729e32a
Raghavan <raghavan.work@outlook.com>
2024-04-23 22:18:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,85 +1,122 @@
+//Import Start
 const express = require('express');
 const { generateApiKey } = require('generate-api-key');
 const { sendmail } = require('./mail.js');
-const {bodyfun} = require('./Body.js')
+const { bodyfun } = require('./Body.js')
 const rateLimit = require("express-rate-limit");
 const requestIP = require('request-ip');
 const app = express();
-const port = 1234;
 var fs = require('fs');
+const { Console } = require('console');
+//Import End
 var currentDate = new Date();
+//Port Start
+const port = 1234;
+//Port End
+const ratelimit = 3;
+// Temp Api Key Start
+const apiKey = 'UQc8Tp1d9elWAh7KDIMkjz2moFs'; 
+const contas = "dsfsdfsdfsdf"
+// Temp Api Key End
 
-// API key for authentication
-const apiKey = 'UQc8Tp1d9elWAh7KDIMkjz2moFs'; // Change this to your actual API key
-const contas = "The 704 and its successors have a 36-bit word length and a 15-bit address space. These computers had two instruction formats, one of which, the Type A, had a short, 3-bit, operation code prefix and two 15-bit fields separated by a 3-bit tag. The first 15-bit field was the operand address and the second held a decrement or count. The tag specified one of three index registers. Indexing was a subtractive process on the 704, hence the value to be loaded into an index register was called a.[2]: p. 8  The 704 hardware had special instructions for accessing the address and decrement fields in a word.[2]: p. 26  As a result it was efficient to use those two fields to store within a single word the two pointers needed for a list"
-// Middleware function to verify API key
 
-
+// Rate Limiter Start
 const limiter = rateLimit({
-  max: 3,
+  max: ratelimit,
   windowMs: 5000,
   message: "Too many request from this IP try again after 10s",
-  handler: (req, res, next, options) =>{res.status(options.statusCode).send(options.message)
-    writeattacklog(gettime()+","
-    +requestIP.getClientIp(req))}  ,
-  
+  handler: (req, res, next, options) => {
+    res.status(options.statusCode).send(options.message)
+    writeattacklog(gettime() + ","
+      + requestIP.getClientIp(req))
+  },
+
 });
 app.use(limiter);
-const verifyApiKey = (req, res, next) => {
-  // Get API key from request header
-  const apiKeyFromRequest = req.headers['x-api-key'];
+// Rate Limiter End
 
+//API Key Verify Start
+const verifyApiKey = (req, res, next) => {
+  const apiKeyFromRequest = req.headers['x-api-key'];
   if (!apiKeyFromRequest || apiKeyFromRequest !== apiKey) {
     return res.status(401).json({ message: 'Unauthorized' });
   }
-
-  next(); 
+  next();
 };
+//API Key Verify End
 
+// Server Listener Start
+app.listen(port, () => console.log('Mail Server is Started at', port));
+// Server Listener End
 
+//Send Mail Use Post Start
+app.post('/api/send-mail', verifyApiKey, async (req, res) => {
 
-// Protected route - requires API key verification
-app.post('/mail', verifyApiKey,async  (req, res) => {
-   
-    if(req.headers['to-mail']!=null){ 
-    //  await sendmail(req.headers['to-mail'].toString(),"testing",bodyfun("Fun Company",contas));
-    writelog(gettime()+","+req.headers['to-mail']+","+req.headers['x-api-key']+","+requestIP.getClientIp(req))
+  if (req.headers['to-mail'] != null) {
+
+    mailselector(req.headers['mail-code'], req)
     res.send({
       message: 'Mail Sended To The Client',
     });
-    }else{
-      res.send({
-        message: 'Invaild Header',
-      });
-    }
-    
+  } else {
+    res.send({
+      message: 'Invaild Header',
+    });
+  }
+
 });
+//Send Mail Use Post End
 
+//Mail Selector Start
+function mailselector(incode, req) {
+  if (incode == 0) {
+    //ex:signup email
+    //  await sendmail(req.headers['to-mail'].toString(),"testing",bodyfun("Fun Company",contas));
+    writelog(gettime() + "," + req.headers['to-mail'] + "," + req.headers['x-api-key'] + "," + requestIP.getClientIp(req))
+    return
+  }
+  if (incode == 1) { //ex:loginmail email
 
-app.listen(port, () => console.log('Mail Server is Started at', port));
+    return
+  }
+  return
+}
+//Mail Selector End
 
-const writelog=(logmsg)=>{
+//Log Saving Start
+const writelog = (logmsg) => {
  
-  fs.appendFile('./Logs/'+currentDate.toDateString()+'.csv', logmsg.toString()+'\n', function (err) {
+  fs.appendFile('./Logs/' + currentDate.toDateString() + '.csv', logmsg.toString() + '\n', function (err) {
     if (err) throw err;
-    console.log(gettime()+' Log Saved');
+    if(err!=null){
+      ratelimit=0;
+      Console.log("Error Is Logs Storage",e)
+    }
+    console.log(gettime() + ' Log Saved');
+  });
+ 
+ 
+ 
+}
+const writeattacklog = (logmsg) => {
+
+  fs.appendFile('./Logs/AttackLogs/' + currentDate.toDateString() + '.csv', logmsg.toString() + '\n', function (err) {
+    if (err) throw err;
+    console.log(gettime() + ' AttackLogs Saved');
   });
 }
-const writeattacklog=(logmsg)=>{
- 
-  fs.appendFile('./Logs/AttackLogs/'+currentDate.toDateString()+'.csv', logmsg.toString()+'\n', function (err) {
-    if (err) throw err;
-    console.log(gettime()+' AttackLogs Saved');
-  });
-}
-function gettime(){
+//Log Saving End
+//Timing Funtion Start
+
+function gettime() {
   var hours = currentDate.getHours();
   var minutes = currentDate.getMinutes();
   var seconds = currentDate.getSeconds();
-  
+
   hours = (hours < 10 ? "0" : "") + hours;
   minutes = (minutes < 10 ? "0" : "") + minutes;
   seconds = (seconds < 10 ? "0" : "") + seconds;
   var currentTime = hours + ":" + minutes + ":" + seconds;
- return  currentTime
+  return currentTime
 }
+//Timing Funtion End
